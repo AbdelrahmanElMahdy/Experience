36d02e1987b32dad417254b7972427a2bce68480
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-20 14:04:25 +0200

    intial commit

---
+++ User/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import (AbstractUser,BaseUserManager)
+
+class UserManager(BaseUserManager):
+    def create_user(self,email,password=None):
+        if not email:
+            raise ValueError("you must provid email address")
+
+        user=self.model(
+            email=self.normalize_email(email),
+        )
+        
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+    
+    def create_superuser(self, email, password=None):
+
+        user=self.create_user(email=email,password=password)
+        user.is_admin=True
+        user.is_stuff=True
+
+        user.save(using=self._db)
+        
+        return user
+
+
+class User(AbstractUser):
+    email=models.EmailField(
+        verbose_name='Email Address',
+        max_length=254,
+        unique=True
+    )
+    is_active=models.BooleanField(default=True)
+    is_stuff=models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects= UserManager()
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+       return self.is_admin
+    def has_perm(self, perm, obj=None):
+       return self.is_admin
