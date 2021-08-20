60e04ee8c5f3eb4c60faa9fb1374184b5fae95f8
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-20 14:31:48 +0200

    header: JWT body: automatically generate token for user when sign in. footer: MHDI

--- User/models.py
+++ User/models.py
@@ -51,3 +51,8 @@ class User(AbstractUser):
        return self.is_admin
     def has_perm(self, perm, obj=None):
        return self.is_admin
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
