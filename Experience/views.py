1478d596c7f890feb99b90e50acf33f9c305b611
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-21 22:58:55 +0200

    header:  modifing experience app's views body: 1- function to list all experience       2- function to create new exp footer:

--- Experience/views.py
+++ Experience/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Experience
+from .serializers import CreatExpSerializer
+from rest_framework.permissions import IsAdminUser,IsAuthenticated,AllowAny
 # Create your views here.
+
+class ExpList(generics.ListAPIView):
+    queryset=Experience.objects.all()
+    serializer_class=CreatExpSerializer
+    permission_classes = [IsAuthenticated]
+
+class CreateExp(generics.CreateAPIView):
+    queryset=Experience.objects.all()
+    serializer_class=CreatExpSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self,serializer):
+        serializer.save(author=self.request.user)
