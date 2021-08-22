da6c853bef87804a68ce659489976200ca0d7fcd
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-22 21:22:11 +0200

    header: anchor for rate crud

--- Experience/urls.py
+++ Experience/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path("list/",ExpList.as_view(),name='list'),
     path("createexp/",CreateExp.as_view(),name='CreateExp'),
     path("ExpUpdate/<int:pk>/",ExpUpdate.as_view(),name='ExpUpdate'),
-
-
+    path("ExpRateList/<int:pk>/",ExpRateList.as_view(),name='ExpRateList'),
+    path("CreateRate/<int:pk>/",CreateRate.as_view(),name='CreateRate'),
+    path("RemoveRate/<int:pk>/",RemoveRate.as_view(),name='RemoveRate'),
+    path("RateUpdate/<int:pk>/",RateUpdate.as_view(),name='RateUpdate'),
 
 ]
