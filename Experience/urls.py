aac077df221afce019d74c83a38dd76989286168
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-22 12:39:11 +0200

    header: anchor to destroyExperience body: set anchor to destroyExperience footer:

--- Experience/urls.py
+++ Experience/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
-from .views import ExpList,CreateExp
+from .views import ExpList,CreateExp,RemoveExp
 
 app_name="experience"
 
 urlpatterns = [
     path("list/",ExpList.as_view(),name='list'),
-    path("createexp/",CreateExp.as_view(),name='CreateExp')
+    path("createexp/",CreateExp.as_view(),name='CreateExp'),
+    path("Removeexp/<int:pk>/",RemoveExp.as_view(),name='RemoveExp')
+
 
 ]
