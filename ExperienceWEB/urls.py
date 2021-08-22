3bee63369fe37e87258133b0ccac10dbf9f800b2
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-22 12:41:55 +0200

    header: editing vc settings , web urls.py body: 1-enable auto complete       2-set index page for the website footer

--- ExperienceWEB/urls.py
+++ ExperienceWEB/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from User.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index),
     path("user/", include("User.urls") ),
-    path("experience/", include("Experience.urls") )
-
+    path("experience/", include("Experience.urls") ),
+    
 ]
