f8902859a27dbdd9545b7505bc8b64c7386df803
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-22 12:37:56 +0200

    header: destroy Experience body: create destroy Experience footer:

--- Experience/views.py
+++ Experience/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated,AllowAny
+from rest_framework import status
+
+
 from .models import Experience
 from .serializers import CreatExpSerializer
-from rest_framework.permissions import IsAdminUser,IsAuthenticated,AllowAny
 # Create your views here.
 
 class ExpList(generics.ListAPIView):
@@ -17,3 +22,25 @@ class CreateExp(generics.CreateAPIView):
 
     def perform_create(self,serializer):
         serializer.save(author=self.request.user)
+
+class RemoveExp(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        expInstance=Experience.objects.filter(pk=self.kwargs['pk'])
+        return expInstance
+    
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance=self.get_queryset()[0]
+        except:
+            return Response("Not Found", status=status.HTTP_404_NOT_FOUND)
+
+        
+        if self.request.user==instance.author:
+            instance.delete()
+        
+        else:
+            return Response("unauthorized operation",status=status.HTTP_401_UNAUTHORIZED)
+            
