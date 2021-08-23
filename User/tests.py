faff15610e38ef799173ea83dd14c23c3ba4bfb2
abdelrahmanelmahdy <a.b.mahdey@gmail.com>
2021-08-23 12:29:41 +0200

    testing user app

--- User/tests.py
+++ User/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import User
 # Create your tests here.
+
+class UserTestCases(TestCase):
+    def setUp(self):
+        User.objects.create_user(
+            email="adam@gmail.com",
+            username="adam",
+            password="testpassword",
+        )
+     
+        User.objects.create_user(
+            email="murray@gmail.com",
+            username="murray",
+            password="testpassword",
+        )
+
+    def test_user(self):
+        adam=User.objects.get(email="adam@gmail.com")
+        murray=User.objects.get(email="murray@gmail.com")
+
+        self.assertEqual(adam.username, 'adam')
+        self.assertEqual(murray.username, 'murray')
